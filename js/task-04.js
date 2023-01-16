637be6fc69332fb0bb23420d78be65ec04276f79
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-01-16 15:27:05 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,33 +1,13 @@
-const counterValue = {
-	value: 0,
-	increment() {
-		console.log('increment -> this', this);
-		this.value += 1;
-	},
-	decrement() {
-		console.log('decrement -> this', this);
-		this.value -= 1;
-	},
-};
-
-
-
 const decrementBtn = document.querySelector('#counter button[data-action="decrement"]');
 const valueEl = document.querySelector('#value');
 const incrementBtn = document.querySelector('#counter button[data-action="increment"]');
 
-decrementBtn.addEventListener('click', () => {
-	console.log('клик по кнопке минус');
+let counterValue = 0;
 
-	counterValue.decrement();
-	console.log(counterValue);
-	valueEl.textContent = counterValue.value;
+decrementBtn.addEventListener('click', () => {
+	valueEl.textContent = counterValue -= 1;
 });
 
 incrementBtn.addEventListener('click', () => {
-	console.log('клик по кнопке плюс');
-
-	counterValue.increment();
-	console.log(counterValue);
-	valueEl.textContent = counterValue.value;
+	valueEl.textContent = counterValue += 1;
 });
