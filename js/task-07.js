5283f5ebe3b8cee57d4dbec0a2581f56f718a228
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-01-17 13:04:44 +0200

    editte task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,19 @@
+// Черновик ->
+// const inputFontSize = document.getElementById('font-size-control');
+// console.log(input);
+// const spanText = document.getElementById('text');
+// console.log(spanText);
+
+// input.addEventListener('input', handleInputRange);
+// function handleInputRange(event) {
+// 	span.style.fontSize = event.currentTarget.value + "px";
+// }
+
 const inputFontSize = document.querySelector("#font-size-control");
 const spanText = document.querySelector("#text");
 
-const handleInput = () => {
+const handleInputRange = () => {
   spanText.style.fontSize = `${inputFontSize.value}px`;
 };
 
-inputFontSize.addEventListener("input", handleInput);
+inputFontSize.addEventListener("input", handleInputRange);
