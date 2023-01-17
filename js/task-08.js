086103807ca40d6ab82540f07695b63fb82a013c
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-01-17 16:12:16 +0200

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+const onFormSubmin = (event) => {
+	event.preventDefault();
+
+	const email = event.currentTarget.elements.email;
+	const password = event.currentTarget.elements.password;
+
+	if (email.value === '' || password.value === '') {
+		alert('Користувач, всі поля мають бути заповнені!');
+	} else {
+		event.email = email.value;
+		event.password = password.value;
+	}
+	console.log({email: email.value,
+		password: password.value });
+	event.currentTarget.reset();
+};
+form.addEventListener('submit', onFormSubmin);
