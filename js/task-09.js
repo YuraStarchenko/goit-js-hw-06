cfcd647be4007d2ef0806bc2487dbd3e95fa6287
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-01-18 13:18:10 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const widget = {
+	changeColorsBtn: document.querySelector('.change-color'),
+	body: document.querySelector('body'),
+	spanColor: document.querySelector('.color'),
+};
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+widget.changeColorsBtn.addEventListener('click', () => {
+	widget.body.style.backgroundColor = getRandomHexColor();
+	widget.spanColor.textContent = widget.body.backgroundColor;
+});
