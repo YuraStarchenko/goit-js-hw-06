d5cb75dfd9b4efde9a3607502598124a04fdb57f
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-01-19 15:40:30 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -22,43 +22,6 @@
 
 // Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
 
-// const refs = {
-//   amountEl: document.querySelector('#controls input'),
-//   createEl: document.querySelector('button[data-create]'),
-//   destroyEl: document.querySelector('button[data-destroy]'),
-//   boxesEl: document.querySelector('#boxes')
-// }
-
-// function createBoxes(amount) {
-//   for(let i = 0; i < amount; i++) {
-//     const elem = document.createElement('div');
-    
-//     elem.style.width = 30 + (10 * i)+'px';
-//     elem.style.height = 30 + (10 * i)+'px';
-//     elem.style.backgroundColor = getRandomHexColor();
-    
-//     refs.boxesEl.append(elem);
-//   }
-// }
-
-// refs.createEl.addEventListener('click', function() {
-//   const amount = refs.amountEl.value;
-//   if(amount > 0) {
-//     createBoxes(amount);
-//   }
-// });
-
-// refs.destroyEl.addEventListener('click', function() {
-//   refs.boxesEl.innerHTML = '';
-// });
-
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
 const refs = {
 	inputRef: document.querySelector('input[type="number"]'),
 	renderBtnRef: document.querySelector("[data-create]"),
